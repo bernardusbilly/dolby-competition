da4ca414418304566328f3903f6a5cdbbbb9745b
bernardusbilly <bernardus.billy@hotmail.com>
2016-04-29 04:21:45 -0700

    Added animation for dolby

--- build/js/scripts2.js
+++ build/js/scripts2.js
@@ -24,15 +24,28 @@ $(document).ready(function() {
 		audio.pause();
 	});
 
+	var stillInHover = true;
 	$('#dolby').hover(function() {
 		if (ddolby === true) {
-			audio = new Audio("build/audio/" + this.id+ "_Dolby.mp4");	
+			audio = new Audio("build/audio/" + this.id+ "_Dolby.mp4");
 		} else {
 			audio = new Audio("build/audio/" + this.id+ ".mp3");	
 		}
 		audio.loop = true;
 		audio.play()
+
+		stillInHover = true;
+		$('.hexagon').addClass("active");
+		setTimeout(function() {
+			$('.hexagon').removeClass("active");
+			if (stillInHover == true) {
+				$('.hexagon').addClass("active2");
+			}
+		}, 13000);
 	}, function() {
 		audio.pause();
+		stillInHover = false;
+		$('.hexagon').removeClass("active");
+		$('.hexagon').removeClass("active2");
 	});
 });
